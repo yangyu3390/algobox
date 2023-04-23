98976a4c99e0f9f0773d4acca91a14ae2d20f896
yangyu3390 <yangyu3390@gmail.com>
2023-04-24 07:46:53 +1000

    trie

---
+++ trie/src/include/trie.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+namespace trie {
+
+template <typename T>
+class TrieNode {
+
+};
+
+template <typename T>
+class TrieValueNode {
+
+};
+
+template <typename T>
+class Trie {
+
+};
+    
+}
