bd65d9df12b17cc465c40ab7afad70fac6f3d724
yangyu3390 <yangyu3390@gmail.com>
2023-05-24 07:30:48 +1000

    define member functions

--- trie/src/include/trie.h
+++ trie/src/include/trie.h
@@ -11,11 +11,58 @@ namespace trie {
 
 template <typename T>
 class TrieNode {
+    public:
+    explicit TrieNode(char key) {
+        m_key = key;
+    }
 
+    TrieNode(TrieNode&& other) {
+        m_key = other.key;
+        m_children = std::move(other.m_children);
+    }
+
+    virtual ~TrieNode() = default;
+
+    bool HasChild(char key) const {
+
+    }
+
+    bool HasChidlren() const {
+
+    }
+
+    bool IsEndNode() const { 
+        return is_end_;
+    }
+
+    bool IsEndNode() const { 
+        return is_end_;
+    }
+
+    InsertChildNode() {
+
+    }
+
+    GetChildNode() {
+
+    }
+
+    RemoveChildNode() {
+
+    }
+
+    SetEndNode() {
+
+    }
+
+    protected:
+    std::unordered_map<char, std::unique_ptr<TrieNode>> m_children;
+    char m_key;
+    bool m_is_end{false};
 };
 
 template <typename T>
-class TrieValueNode {
+class TrieNodeWithValue : public TrieNode {
 
 };
 
